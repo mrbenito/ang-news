2aeb9e983156845a1fb58e93ca4d0053c523f816
Mr. Benito <bsegovia@gmail.com>
2014-09-29 01:29:47 -0500

    pushing current state of tutorial. Up to https://thinkster.io/angulartutorial/learn-to-build-realtime-webapps/#accessing-the-current-user-throughout-the-application-8

---
+++ app/scripts/app.js
@@ -0,0 +1,45 @@
+/* global app:true */
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angNewsApp
+ * @description
+ * # angNewsApp
+ *
+ * Main module of the application.
+ */
+var app = angular.module('angNewsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase'
+
+  ])
+  .constant('FIREBASE_URL', 'https://ang-news20140921.firebaseIO.com/');
+
+  app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
+      })
+      .when('/posts/:postId', {
+        templateUrl: 'views/showpost.html',
+        controller: 'PostViewCtrl'
+      })
+      .when('/register/', {
+        templateUrl: 'views/register.html',
+        controller: 'AuthCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'AuthCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/services/post.js
@@ -0,0 +1,23 @@
+'use strict';
+
+app.factory('Post', 
+	function ($firebase, FIREBASE_URL) {
+		var ref = new Firebase(FIREBASE_URL + 'posts');
+
+		var posts = $firebase(ref).$asArray();
+
+		var Post = {
+			all: posts,
+			create: function (post) {
+				return posts.$add(post);
+			},
+			find: function (postId) {
+				return $firebase(ref.child(postId)).$asObject();
+			},
+			delete: function (post) {
+				return posts.$remove(post);
+			}
+		};
+
+		return Post;
+	});
